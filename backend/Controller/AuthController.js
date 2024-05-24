d25db29379a47dcd851b3ed39b47f3a42eecd763
harish <harishnagubadi9190@gmail.com>
2024-05-24 20:11:52 +0530

    first commit1

--- backend/Controller/AuthController.js
+++ backend/Controller/AuthController.js
@@ -1,24 +1,55 @@
 const UserModel = require("../Models/UserModel")
-
+const bcrypt = require("bcryptjs")
 
 
 async function signUp(req,res) {
 
     try
     {
-        const {fullName,username,password,gender,profilePic} = req.body
+        const {fullName,username,password, confirmPassword,gender,profilePic} = req.body
 
-        const userData = await new UserModel({
+        if(password !== confirmPassword)
+            {
+                return res.json("passwords are not matching")
+            }
+
+        const userExist = await UserModel.findOne({username})
+        if(userExist)
+            {
+                return res.json({msg : "user already exist"})
+            }
+
+            const salt = await bcrypt.genSalt(10)
+            const hassPassword = await bcrypt.hash(password, salt)
+
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+
+        
+        const newUser = await new UserModel({
             fullName,
             username,
-            password,
+            password : hassPassword,
             gender,
-            profilePic
+            profilePic : gender === "male" ? boyProfilePic : girlProfilePic  
         })
 
-        const d = await userData.save()
-        return res.json(d)
+        if(newUser)
+            {
 
+                await newUser.save()
+                return res.json({
+                    fullName : newUser.fullName,
+                    username : newUser.username,
+                    profilePic: newUser.profilePic 
+                })
+            }
+        else
+        {
+            return res.json({msg : "invalid data"})
+        }
     }
 
 
